c1637baa6663a593643dd788f5abd7afc6a9a3c7
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-09-19 15:04:55 +0200

    added site info

---
+++ assignments/wpa/wpa0WhiteWalter.R
@@ -0,0 +1,11 @@
+# Assignment: WPA 0
+# Name: Walter White
+# Date: 22 February 2017
+
+## 1A: # Install and load the yarrr package
+install.packages('yarrr')
+library(yarrr)
+
+## 2A: Get help for pirates data
+?pirates
+
